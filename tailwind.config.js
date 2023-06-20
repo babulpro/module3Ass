8a2e1acbd850ec77258018376496ff9dd658ea03
babulpro <sh8657706@gmail.com>
2023-06-20 16:05:57 +0600

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}","index.html","ass1.html","ass2.html","ass3.html","ass4.html","ass5.html","ass6.html","ass7.html","ass8.html","ass9.html","ass10.html","ass11.html","ass12.html","ass13.html","ass14.html","ass15.html"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
